8cf65040cd01d546d7475016cd96a3c387bde2be
marput <marcin.putra@gmail.com>
2023-12-23 19:34:48 +0100

    Dodanie zakończenia zdarzenia.

--- data/database_user.sql
+++ data/database_user.sql
@@ -45,9 +45,11 @@ CREATE TABLE "reports"
     FOREIGN KEY ("id_events") REFERENCES "events" ("id_events")
 );
 
+
+
 INSERT INTO "users"
-VALUES  (NULL, 'user', 'userLast', 'user', '1234', 1, false),
-        (NULL, 'admin', 'userLast', 'admin', '1234', 1, true);
+VALUES  (NULL, 'Jan', 'Nowak', 'user', 'scrypt:32768:8:1$m2MIEMl5mQTJx2js$c596ac7662dcc4db97c8370487879718c0478f565fd468e5fdb965269c146d2bf723e2bbe8ebbc61be7821bf91847bd5f0697d2823c2a3874a6e17c7f8c5ac43', 1, false),
+        (NULL, 'Piotr', 'Kowalski', 'admin', 'scrypt:32768:8:1$m2MIEMl5mQTJx2js$c596ac7662dcc4db97c8370487879718c0478f565fd468e5fdb965269c146d2bf723e2bbe8ebbc61be7821bf91847bd5f0697d2823c2a3874a6e17c7f8c5ac43', 1, true);
 
 INSERT INTO "organization"
 VALUES (NULL, 'Biuro1', 'Polna 1', 'Sopot');
@@ -58,6 +60,8 @@ VALUES  (NULL, 'Naprawa PC'),
         (NULL, 'Instalacja oprogramowania');
 
 INSERT INTO "reports"
-VALUES  (NULL, 1, 1, '2012-12-02', 'Otwarte', 'sala 1', 'Opis zadania nr 1','540123123','user@mail.pl'),
-        (NULL, 3, 1, '2012-12-06', 'Zamknięte', 'sala 6', 'Opis zadania nr 3','670124543','admin@mail.com'),
-        (NULL, 2, 2, '2013-01-08', 'Otwarte', 'sala 3', 'Opis zadania nr 2','600100100','user@domena.pl');
+VALUES  (NULL, 1, 1, '2022-12-02', 'Otwarte', 'sala 1', 'Opis zadania nr 1','540123123','user@mail.pl'),
+        (NULL, 2, 1, '2023-12-06', 'Zamknięte', 'sala 6', 'Opis zadania nr 2','670124543','admin@mail.com'),
+        (NULL, 3, 2, '2023-09-20', 'Otwarte', 'sala 3', 'Opis zadania nr 3','567780018','main@abc.pl'),
+        (NULL, 1, 1, '2023-10-10', 'Otwarte', 'sala 3', 'Opis zadania nr 4','687298345','root@poland.pl'),
+        (NULL, 2, 2, '2023-11-09', 'Otwarte', 'sala 3', 'Opis zadania nr 5','456012892','port@wer.pl');
